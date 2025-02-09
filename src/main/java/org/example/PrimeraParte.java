cca9cacd86696182a5b33dc339a7be3144f615d4
Juanpapb0401 <e5jparra@gmail.com>
2025-02-09 18:17:15 -0500

    Rename

--- src/main/java/org/example/PrimeraParte.java
+++ src/main/java/org/example/PrimeraParte.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class PrimeraParte {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8080);
 
